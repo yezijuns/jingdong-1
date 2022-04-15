31231b76bd5005f07ae191fc4d9df72c8c30947b
yezijuns <1559411344@qq.com>
2022-04-15 21:24:07 +0800

    shop-search

--- src/views/shop/Shop.vue
+++ src/views/shop/Shop.vue
@@ -1,15 +1,30 @@
 <template>
   <div class="wrapper">
+    <div class="search">
+      <div
+       class="search__back iconfont"
+       @click="handleBackClick"
+      >&#xe679;</div>
+      <div class="search__content">
+        <div class="search__content__icon iconfont">&#xe600;</div>
+        <input
+         class="search__content__input"
+         placeholder="请输入商品名称"
+        >
+      </div>
+    </div>
     <ShopInfo :item = "item" :hideBorder="true"/>
   </div>
 </template>
 
 <script>
+import { useRouter } from 'vue-router'
 import ShopInfo from '../../components/ShopInfo'
 export default {
   name: 'Shop',
   components: { ShopInfo },
   setup () {
+    const router = useRouter()
     const item = {
       _id: '1',
       name: '沃尔玛',
@@ -19,7 +34,10 @@ export default {
       expressPrice: 5,
       slogan: 'VIP尊享满89元减4元运费券'
     }
-    return { item }
+    const handleBackClick = () => {
+      router.back()
+    }
+    return { item, handleBackClick }
   }
 }
 </script>
@@ -27,5 +45,42 @@ export default {
 <style lang="scss" ecoped>
 .wrapper {
   padding: 0 .18rem;
+}
+.search {
+  display: flex;
+  padding: .2rem 0 .16rem 0;
+  &__back {
+    line-height: .32rem;
+    width: .3rem;
+    font-size: .24rem;
+    color:  #B6B6B6;
+  }
+  &__content {
+    display: flex;
+    flex: 1;
+    background: #F5F5F5;
+    border-radius: .16rem;
+    &__icon {
+      width: .44rem;
+      text-align: center;
+      font-size: .2rem;
+      margin: .08rem 0.04rem .08rem .1rem;
+    }
+    &__input {
+      display: block;
+      padding-right: .2rem;
+      width: 100%;
+      height: .32rem;
+      background: none;
+      border: none;
+      outline: none;
+      margin-top: .03rem;
+      font-size: .14rem;
+      color: #333;
+      &::placeholder {
+        color: #333;
+      }
+    }
+  }
 }
 </style>
