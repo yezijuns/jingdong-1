1ae0889e85170cb650d5d82134ec433f1f4ef785
yezijuns <1559411344@qq.com>
2022-04-24 21:52:52 +0800

    product-check

--- src/views/shop/Cart.vue
+++ src/views/shop/Cart.vue
@@ -1,33 +1,41 @@
 <template>
 <div class="cart">
   <div class="product">
-      <template
-        v-for="item in productList"
-        :key="item._id"
-      >
-        <div class="product__item" v-if="item.count > 0">
-          <img class="product__item__img" :src="item.imgUrl" />
-          <div class="product__item__detail">
-            <h4 class="product__item__title">{{item.name}}</h4>
-            <p class="product__item__price">
-              <span class="product__item__yen">&yen;</span>{{item.price}}
-              <span class="product__item__origin">&yen;{{item.oldPrice}}</span>
-            </p>
-          </div>
-          <div class="product__number">
-            <span
-             class="product__number__minus"
-             @click="() => { changeItemInfo(shopId, item._id, item, -1) }"
-            >-</span>
-              {{item.count || 0}}
-            <span
-              class="product__number__plus"
-              @click="() => { changeItemInfo(shopId, item._id, item, 1) }"
-            >+</span>
-          </div>
-        </div>
-      </template>
+    <div class="product__header">
+
     </div>
+    <template
+      v-for="item in productList"
+      :key="item._id"
+    >
+      <div class="product__item" v-if="item.count > 0">
+        <div
+         class="product__item__checked iconfont"
+         v-html = "item.check ? ' &#xe6f7; ' : '&#xe731;' "
+         @click="() => changeCartItemCheck(shopId, item._id)"
+        />
+        <img class="product__item__img" :src="item.imgUrl" />
+        <div class="product__item__detail">
+          <h4 class="product__item__title">{{item.name}}</h4>
+          <p class="product__item__price">
+            <span class="product__item__yen">&yen;</span>{{item.price}}
+            <span class="product__item__origin">&yen;{{item.oldPrice}}</span>
+          </p>
+        </div>
+        <div class="product__number">
+          <span
+           class="product__number__minus"
+           @click="() => { changeItemInfo(shopId, item._id, item, -1) }"
+          >-</span>
+            {{item.count || 0}}
+          <span
+            class="product__number__plus"
+            @click="() => { changeItemInfo(shopId, item._id, item, 1) }"
+          >+</span>
+        </div>
+      </div>
+    </template>
+  </div>
   <div class="check">
     <div class="check__icon">
       <img
@@ -52,6 +60,7 @@ import { useCommonCartEffect } from './commonCartEffect'
 
 // 获取购物车信息逻辑
 const useCartEffect = (shopId) => {
+  const { changeItemInfo } = useCommonCartEffect()
   const store = useStore()
   const cartList = store.state.cartList
   const total = computed(() => {
@@ -71,7 +80,9 @@ const useCartEffect = (shopId) => {
     if (productList) {
       for (const i in productList) {
         const product = productList[i]
-        count += (product.count * product.price)
+        if (product.check) {
+          count += (product.count * product.price)
+        }
       }
     }
     return count.toFixed(2)
@@ -82,7 +93,11 @@ const useCartEffect = (shopId) => {
     return productList
   })
 
-  return { total, price, cartList, productList }
+  const changeCartItemCheck = (shopId, productId) => {
+    store.commit('changeCartItemChecked', { shopId, productId })
+  }
+
+  return { total, price, cartList, productList, changeItemInfo, changeCartItemCheck }
 }
 
 export default {
@@ -90,9 +105,8 @@ export default {
   setup () {
     const route = useRoute()
     const shopId = route.params.id
-    const { changeItemInfo } = useCommonCartEffect()
-    const { total, price, productList } = useCartEffect(shopId)
-    return { total, price, shopId, productList, changeItemInfo }
+    const { total, price, productList, changeItemInfo, changeCartItemCheck } = useCartEffect(shopId)
+    return { total, price, shopId, productList, changeItemInfo, changeCartItemCheck }
   }
 }
 </script>
@@ -160,12 +174,22 @@ export default {
   overflow-y: scroll;
   flex: 1;
   background: #FFF;
+  &__header {
+    height: .52rem;
+    border-bottom:1px solid #F1F1F1;
+  }
   &__item {
     position: relative;
     display: flex;
     padding: .12rem 0;
     margin: 0 .16rem;
     border-bottom: .01rem solid $content-bgColor;
+    &__checked {
+      line-height: .5rem;
+      margin-right: .2rem;
+      color: #0091FF;
+      font-size: .2rem;
+    }
     &__img {
       width: .46rem;
       height: .46rem;
--- src/views/shop/Content.vue
+++ src/views/shop/Content.vue
@@ -30,7 +30,7 @@
            class="product__number__minus"
            @click="() => { changeItemInfo(shopId, item._id, item, -1) }"
           >-</span>
-            {{cartList?.[shopId]?.[item._id]?.count || 0}}
+            {{item.count || 0}}
           <span
             class="product__number__plus"
             @click="() => { changeItemInfo(shopId, item._id, item, 1) }"
@@ -85,9 +85,9 @@ export default {
     const shopId = route.params.id
     const { currentTab, handleTabClick } = useTabEffect()
     const { list } = useCurrentListEffect(currentTab, shopId)
-    const { cartList, changeItemInfo } = useCommonCartEffect()
+    const { changeItemInfo } = useCommonCartEffect()
     return {
-      categories, currentTab, handleTabClick, list, cartList, shopId, changeItemInfo
+      categories, currentTab, handleTabClick, list, shopId, changeItemInfo
     }
   }
 }
