9ce276e7400539320838c5861f9c4cc19bff24fe
yezijuns <1559411344@qq.com>
2022-04-30 23:23:46 +0800

    orderComfirmation-2

--- src/views/orderConfirmation/OrderConfirmation.vue
+++ src/views/orderConfirmation/OrderConfirmation.vue
@@ -22,3 +22,25 @@ export default {
   name: 'OrderConfirmation'
 }
 </script>
+
+<style lang="scss" scoped>
+.top {
+  position: relative;
+  height: 1.96rem;
+  background-size: 100% 1.59rem;
+  background-image: linear-gradient(0deg, rgba(0,145,255,0.00) 4%, #0091FF 50%);
+  background-repeat: no-repeat;
+  &__header {
+    padding-top: .4rem;
+    line-height: .24rem;
+    color: #FFF;
+    text-align: center;
+    font-size: .16rem;
+    &__back {
+      position: absolute;
+      left: .18rem;
+      font-size: .22rem;
+    }
+  }
+}
+</style>
