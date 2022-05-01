bd8e91c7ce12e18266cbacf84f6f63b4c74dde3c
yezijuns <1559411344@qq.com>
2022-05-01 13:18:17 +0800

    orderConfirmation-2

--- src/views/orderConfirmation/OrderConfirmation.vue
+++ src/views/orderConfirmation/OrderConfirmation.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="top">
-    <div class="top__bgcor" />
-    <div class="top__header">
-      <div class="iconfont top__header__back">&#xe679;</div>
-      确认订单
-    </div>
-    <div class="top__receiver">
-      <div class="top__receiver__title">收货地址</div>
-      <div class="top__receiver__title"> 北京理工大学国防科技园2号楼10层 </div>
-      <div class="top__receiver__info">
-        <span class="top__receiver__info__name">瑶妹（先生）</span>
-        <span class="top__receiver__info__name">18911024266</span>
+  <div class="wrapper">
+    <div class="top">
+      <div class="top__bgcor" />
+      <div class="top__header">
+        <div class="iconfont top__header__back">&#xe679;</div>
+        确认订单
       </div>
-      <div class="iconfont top__receiver__enter">&#xe679;</div>
+      <div class="top__receiver">
+        <div class="top__receiver__title">收货地址</div>
+        <div class="top__receiver__address"> 北京理工大学国防科技园2号楼10层 </div>
+        <div class="top__receiver__info">
+          <span class="top__receiver__info__name">瑶妹（先生）</span>
+          <span class="top__receiver__info__name">18911024266</span>
+        </div>
+        <div class="iconfont top__receiver__icon">&#xe679;</div>
+      </div>
     </div>
   </div>
 </template>
@@ -24,6 +26,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.wrapper {
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  background: #eee;
+}
 .top {
   position: relative;
   height: 1.96rem;
@@ -31,7 +41,7 @@ export default {
   background-image: linear-gradient(0deg, rgba(0,145,255,0.00) 4%, #0091FF 50%);
   background-repeat: no-repeat;
   &__header {
-    padding-top: .4rem;
+    padding-top: .26rem;
     line-height: .24rem;
     color: #FFF;
     text-align: center;
@@ -40,6 +50,44 @@ export default {
       position: absolute;
       left: .18rem;
       font-size: .22rem;
+    }
+  }
+  &__receiver {
+    position: absolute;
+    left: .18rem;
+    right: .18rem;
+    bottom: 0;
+    height: 1.11rem;
+    background: #FFF;
+    border-radius: 0.04rem;
+    &__title {
+      line-height: .22rem;
+      padding: .16rem 0 .14rem .16rem;
+      font-size: .16rem;
+      color: #333;
+    }
+    &__address {
+      line-height: .2rem;
+      padding: 0 .4rem 0 .16rem;
+      font-size: .14rem;
+      color: #333;
+    }
+    &__info {
+      padding: 0.06rem 0 0 .16rem;
+      &__name {
+        margin-right: .06rem;
+        line-height: .18rem;
+        font-size: .12rem;
+        color: #666;
+      }
+    }
+    &__icon {
+      transform: rotate(180deg);
+      position: absolute;
+      right: .16rem;
+      top: .5rem;
+      color: #666;
+      font-size: .16rem;
     }
   }
 }
